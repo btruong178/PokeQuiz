362ddbf01d93702227b7381fda2be2ef549ff0d5
btruong178 <briantruong178@gmail.com>
2024-10-29 03:27:44 -0400

    Overhaul to Project
    Added Backend (Express.js)
    
    Created Practice Code for Myself in Backend (Server.js/api.js)
    
    Configured Package.json files (Frontend/Backend/Root)

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const apiroutes = require('./routes/api')
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Backend Server')
+});
+
+app.use('/api', apiroutes);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
--- frontend/src/js/homepage.js
+++ frontend/src/js/homepage.js
@@ -2,12 +2,12 @@ import '../css/Homepage.css'
 import { Link } from 'react-router-dom';
 function Homepage() {
     return (
-        <div className='full-height'>
-            <div className="row">
-                <div className="col-md-2 text-center bg-black">
+        <div className='container-fluid full-height'>
+            <div className="row text-center bg-black">
+                <div className="col-md-6">
                     <h1>My React App</h1>
                 </div>
-                <div className="col-md-2 text-center">
+                <div className="col-md-6">
                     <h1>Homepage</h1>
                     <p>This is the homepage.</p>
                     <Link to='/About'>Go to the about page</Link>
